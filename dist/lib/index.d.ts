302984e754dae800e7e224bc824aefe01dc80cc7
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-02-08 16:53:28 +0100

    add an index so bundlephobia can compute bundle size

---
+++ dist/lib/index.d.ts
@@ -0,0 +1,8 @@
+import * as nsMap from "./Map";
+import * as nsSet from "./Set";
+import * as nsWeakMap from "./WeakMap";
+import "./Array.from";
+import "./Array.prototype.find";
+import "./ArrayBuffer.isView";
+import "./String.prototype.startsWith";
+export { nsMap, nsSet, nsWeakMap };
---
+++ src/lib/index.ts
@@ -0,0 +1,12 @@
+
+//NOTE: Index included only so bundlephobia can compute the bundle size.
+
+import * as nsMap from "./Map";
+import * as nsSet from "./Set";
+import * as nsWeakMap from "./WeakMap";
+import "./Array.from";
+import "./Array.prototype.find";
+import "./ArrayBuffer.isView";
+import "./String.prototype.startsWith";
+
+export { nsMap, nsSet, nsWeakMap };
